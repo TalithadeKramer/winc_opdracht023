9c2941279b4a14f249b26ee0bf047d0af327bd25
TalithadeKramer <76871148+TalithadeKramer@users.noreply.github.com>
2021-03-01 15:44:04 +0100

    Add files via upload

---
+++ Opdracht_023/script.js
@@ -0,0 +1,56 @@
+/*const heat = function (item) {
+    console.log("I'm heating " + item);
+    console.log("Ping! Your " + item + " is heated.")
+};
+
+const mainCourse = "pasta";
+const dessert = "cake";
+
+heat(mainCourse);
+heat(dessert);*/
+
+/*const multiply = function (numberA, numberB) {
+    console.log(numberA * numberB);
+}
+
+multiply(4, 3);
+
+const age = 4;
+const dogYearsFactor = 7;
+multiply(age, dogYearsFactor);*/
+
+/*const calculateAverage = function (numbers) {
+
+    const average = numbers.reduce((total, n) => total + n) / numbers.length;
+    console.log(average)
+};
+
+calculateAverage([1, 2])
+
+const grades = [6, 7, 8, 9, 5, 6, 7, 8];
+calculateAverage(grades);
+
+const ages = [16, 22, 50, 100, 34];
+calculateAverage(ages)*/
+
+/*const paint = function (paint) {
+    console.log("De muur is rood geverfd.")
+};*/
+
+/*const paint = function (paint) {
+    console.log("De muur is " + paint + " geverfd.")
+};
+
+const kleur = "groen";
+
+paint(kleur);*/
+
+const paint = function (item) {
+    console.log("De " + item + "geverfd.")
+};
+
+const kleur = "noord muur is oranje";
+const kleur = "zuid-oorst muur is grijs";
+
+paint(kleur);
+paint(kleur);
